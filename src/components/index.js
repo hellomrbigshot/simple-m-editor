7457d71a31b3d6fddf8edd1963145b5aed75e6df
hellomrbigshot <1994yechen@gmail.com>
2019-02-14 23:26:43 +0800

    update

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,14 @@
 import markdownEditor from './markdownEditor.vue'
 
 // import 方式引入
-export { markdownEditor }
+export default markdownEditor
 
 // Vue.use() 全局注册
-export default {
-  install: function (Vue) {
-    Vue.component('m-editor', markdownEditor)
-  }
-}
+// export default {
+//   install: function (Vue) {
+//     Vue.component('m-editor', markdownEditor)
+//   }
+// }
 
 // cdn 引入
 if (typeof window !== 'undefined' && window.Vue) {
