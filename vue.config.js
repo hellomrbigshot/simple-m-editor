9c4f82643ce04dcdfbef6cf73eea856bb0c9d0a7
hellomrbigshot <1994yechen@gmail.com>
2019-08-01 16:05:40 +0800

    add gzip

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,21 @@
+const CompressionPlugin = require("compression-webpack-plugin");
 module.exports = {
   lintOnSave: false,
   css: {
     extract: true
+  },
+  configureWebpack: () => {
+    let configNew = {};
+    if (process.env.NODE_ENV === 'production') {
+      configNew.plugins = [
+        new CompressionPlugin({
+          test: /\.js$|\.html$|\.css/,
+          threshold: 10240,
+          minRatio: 0.8,
+          deleteOriginalAssets: false
+        })
+      ]
+    }
+    return configNew;
   }
 }
